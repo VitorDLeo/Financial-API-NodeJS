20ec7a7293ccfeb1901370094c8a9612b074a119
VitorDLeo <vitordileo003@gmail.com>
2022-06-04 17:23:00 -0300

    Search Balance Statement

--- src/index.js
+++ src/index.js
@@ -169,4 +169,29 @@ app.get("/account", verifyIfExistsAccountCPF, (request, response) =>{
 
 });
 
+// Delete Account
+app.delete("/account", verifyIfExistsAccountCPF, (request, response) => {
+
+    const { customer } = request;
+
+    //splice
+
+    customers.splice(customer, 1);
+
+    return response.status(200).json(customers);
+
+});
+
+
+app.get("/balance", verifyIfExistsAccountCPF, (request, response) =>{
+
+    const { customer } = request
+
+    const balance = getBalance(customer.statement);
+
+    return response.json(balance);
+
+});
+
+
 app.listen(3333);
